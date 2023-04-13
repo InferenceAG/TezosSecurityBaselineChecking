70812aa18ec376bfa90c67f12fffa7d2e2c1179b
ecabras <126670074+ecabras@users.noreply.github.com>
2023-04-13 13:34:47 +0200

    Absence of default EP
    This testcase suite checks the protocol's behavior in absence of a default entrypoint

---
+++ testcases/execute_test.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+. ../../_framework/init.sh
+. ../../_framework/functions.sh
+
+head -n 1 readme.md
+
+case $1 in
+	*)
+		echo "executing tests for $1"
+        echo "Single EP - Gas exhaustion expected"
+		admin="$($TEZOSCLIENT list known addresses |grep admin |awk '{ print $2}')"
+				
+		$TEZOSCLIENT originate contract singleEP transferring 0 from deploy running singleEP.tz --burn-cap 1 --force >out.tmp 2>&1
+		
+
+		$TEZOSCLIENT transfer 1 from admin to singleEP >result.tmp 2>&1
+
+		### The test should have triggered a gas exhaustion failure due to an infinite loop of self transactions
+		oneTezPos=$(cat result.tmp |grep -n 'Gas limit exceeded during typechecking or execution')
+
+		if [ -z "$oneTezPos" ]; then
+				echo "testcase failed"
+				exit
+		else
+				echo "testcase Succeeded"
+		fi
+		
+
+        echo "Two EP, one default - No issue expected"
+		admin="$($TEZOSCLIENT list known addresses |grep admin |awk '{ print $2}')"
+				
+		$TEZOSCLIENT originate contract twoEP_withDefault transferring 0 from deploy running twoEP_withDefault.tz --burn-cap 0.2 --force >out.tmp 2>&1
+		
+
+		$TEZOSCLIENT transfer 1 from admin to twoEP_withDefault >result.tmp 2>&1
+
+		### The test should run without any problem
+		oneTezPos=$(cat result.tmp |grep -n 'This transaction was successfully applied')
+
+		if [ -z "$oneTezPos" ]; then
+				echo "testcase failed"
+				exit
+		else
+				echo "testcase Succeeded"
+		fi
+		
+
+        echo "Two EP, no default - Failure expected"
+		admin="$($TEZOSCLIENT list known addresses |grep admin |awk '{ print $2}')"
+				
+		$TEZOSCLIENT originate contract twoEP_noDefault transferring 0 from deploy running twoEP_noDefault.tz --burn-cap 0.2 --force >out.tmp 2>&1
+		
+
+		$TEZOSCLIENT transfer 1 from admin to twoEP_noDefault >result.tmp 2>&1
+
+		### The test should run without any problem
+		oneTezPos=$(cat result.tmp |grep -n 'This operation FAILED.')
+
+		if [ -z "$oneTezPos" ]; then
+				echo "testcase failed"
+				exit
+       
+		else
+				echo "testcase Succeeded"
+		fi
+		
+esac
+rm *.tmp
+rm -rf compiled
---
+++ testcases/readme.md
@@ -0,0 +1,16 @@
+# Testcase title: Default entrypoint testing
+
+## Description
+This scenario contains three different contracts, to check how the protocol behaves when looking for the default entrypoint of a contract.
+
+### Structure of the test
+The three contracts are structured in the following way:
+    - singleEP.tz: has only one EP, which was not marked as the default one;
+    - twoEP_withDefault.tz: has two EPs, one of them was marked as the default one
+    - twoEP_noDefault.tz: has two EPs, but none of them was marked as the default one.
+
+### Expected results
+    - singleEP.tz: the protocol should pick the only EP as the default one. The contract attempts a self transfer, triggering the execution of its only EP, which attempts, again, a self transfer. It's an infinite loop leading to a gas exhaustion failure;
+    - twoEP_withDefault.tz: one of the EPs was marked as default, so the test should complete with a success, executing the default EP;
+    - twoEP_noDefault.tz: the test should fail. There is no default EP, so the protocol cannot determine which one to use to resolve the contract call. Instead of picking a random one, the operation fails and reverts.
+
